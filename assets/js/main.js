02a48a3e3cc15bb65af88a01ad5cd91f921868d7
Danilo Aguiar <danilo.aguiar6757@gmail.com>
2022-03-30 16:14:54 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function imgSlider(any) {
+  document.querySelector('.starbucks').src = any;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
